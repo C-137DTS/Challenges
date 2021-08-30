7b99fe1d47954854f6195a15bc661873fee9688e
Juan Pablo <c137dts@gmail.com>
2021-08-30 01:32:17 -0500

    ✨ Find unique (with comments)

--- findUnique/main.js
+++ findUnique/main.js
@@ -4,12 +4,16 @@ KATA:
 */
 
 const findUniq = (arr) => {
+  // Contain: {number: times}. Ej: {1: 5, 2: 1}
   const posibleUniq = {};
+  // Add the numbers and times to object
   arr.forEach((n) =>
     posibleUniq[n] ? (posibleUniq[n] += 1) : (posibleUniq[n] = 1)
   );
+  // Get the numbers
   const numbers = Object.keys(posibleUniq);
 
+  // Return the unique
   return posibleUniq[numbers[0]] > posibleUniq[numbers[1]]
     ? Number(numbers[1])
     : Number(numbers[0]);
